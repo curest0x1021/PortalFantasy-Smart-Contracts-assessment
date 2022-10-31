096f505ee5bd57a38c68f88f9d6c64a6efadc327
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-11-01 03:43:21 +0900

    Implement Porbles NFT contract

---
+++ contracts/Porbles.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./lib/Ownable.sol";
+import "./lib/ERC2981.sol";
+import "./lib/ERC721URIStorage.sol";
+import "./lib/Counters.sol";
+
+contract Porbles is ERC721URIStorage, ERC2981, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Porbles", "PB") {
+        // Set royalte fee as 1%
+        _setDefaultRoyalty(msg.sender, 100);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public view virtual override(ERC721, ERC2981)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+        _resetTokenRoyalty(tokenId);
+    }
+
+    function burnNFT(uint256 tokenId)
+        public onlyOwner
+    {
+        _burn(tokenId);
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) 
+        public onlwOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function mintNFTWithRoyalty(address recipient, string memory tokenURI, address royaltyReceiver, u96 feeNumerator)
+        public onlyOwner
+        returns (uint256)
+    {
+        uint256 tokenId = mintNFT(recipient, tokenURI);
+        _setTokenRoyalty(tokenId, royaltyReceiver, feeNumerator);
+
+        return tokenId;
+    }
+}
